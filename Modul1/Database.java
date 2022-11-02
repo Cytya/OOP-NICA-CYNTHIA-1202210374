7c9c4e4c2605a96fd9b499275d42b38d08c11ede
Cytya <116426818+Cytya@users.noreply.github.com>
2022-11-02 02:37:40 -0700

    Add files via upload

---
+++ Modul1/Database.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    public static void main (String[] arms){
+        System.out.println("Selamat Datang Direstoran EAD");
+        System.out.println("Silahkan Register Terlebih Dahulu");
+        System.out.println("====================================");{
+        }
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    // TODO Create Method to Insert Menu to Database
+
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Databas
+}
---
+++ Modul1/User.java
@@ -0,0 +1,16 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String Nama = ("Masukkan nama:");
+    String No = ("No Handphone :");
+        public void setNama(String nama){
+            this.Nama = nama ;
+        }
+        public void setNo(String no) {
+            this.no = No ;
+        }
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
